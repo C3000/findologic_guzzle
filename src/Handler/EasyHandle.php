135d0f664e03a5026d4887983eca01a5bf33d463
Dominik Brader <d.brader@findologic.com>
2020-05-11 14:11:28 +0200

    Add missing namespaces

--- src/Handler/EasyHandle.php
+++ src/Handler/EasyHandle.php
@@ -50,8 +50,8 @@ final class EasyHandle
 
         // HTTP-version SP status-code SP reason-phrase
         $startLine = explode(' ', array_shift($this->headers), 3);
-        $headers = \GuzzleHttp\headers_from_lines($this->headers);
-        $normalizedKeys = \GuzzleHttp\normalize_header_keys($headers);
+        $headers = \FINDOLOGIC\GuzzleHttp\headers_from_lines($this->headers);
+        $normalizedKeys = \FINDOLOGIC\GuzzleHttp\normalize_header_keys($headers);
 
         if (!empty($this->options['decode_content'])
             && isset($normalizedKeys['content-encoding'])
